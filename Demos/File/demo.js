07aeb454dbebf91633a518519902a1e9f1772ce9
Jan Kassens <jan@kassens.net>
2008-12-10 19:00:54 +0100

    implemented some file functionality

--- Demos/File/demo.js
+++ Demos/File/demo.js
@@ -14,4 +14,14 @@ window.addEvent('domready', function(){
 	log('app-storage:/foo/jan.txt');
 	log('documents:/foo/jan.txt');
 	log('user:/foo/jan.txt');
+
+	var sourceDir = new Directory('app:/Demos');
+	var files = sourceDir.list();
+
+	files = files.map(function(file){
+		return file.getPath();
+	}).join('\n');
+	air.trace(files);
+
+	air.trace(new File('app:/Demos/index.html').read());
 });
--- Source/Data/File.js
+++ Source/Data/File.js
@@ -1,5 +1,85 @@
+var FilesystemObject = new Class({
+
+	options: {
+		create: true,
+		createParents: true // bad naming
+	},
+
+	initialize: function(path, options){
+		this.file = File.resolve(path);
+	},
+
+	move: function(dir){
+		// moves to dir
+		// dir is a string or Directory
+	},
+
+	trash: function(){
+		this.file.moveToTrash();
+	},
+
+	getPath: function(){
+		return this.file.nativePath;
+	}
+
+});
+
+FilesystemObject.create = function(file){
+	if (file.isDirectory) return new Directory(file);
+	return new File(file);
+};
+
+var Directory = new Class({
+
+	Extends: FilesystemObject,
+
+	initialize: function(path, options){
+		this.parent(path, options);
+	},
+
+	// recursive: (default = false)
+	dispose: function(recursive){
+		this.file.deleteDirectory(recursive);
+		return this;
+	},
+
+	// returns an array of <File>s and <Directory>s
+	list: function(){
+		return this.file.getDirectoryListing().map(FilesystemObject.create);
+	}
+
+});
+
 var File = new Class({
-	
+
+	Extends: FilesystemObject,
+
+	initialize: function(path, options){
+		this.parent(path, options);
+	},
+
+	dispose: function(){
+		this.file.deleteFile();
+		return this;
+	},
+
+	// overrides the file
+	write: function(str){
+	},
+
+	// appends str to the end of the file
+	append: function(str){
+	},
+
+	// returns the content
+	read: function(){
+		var stream = new air.FileStream();
+		stream.open(this.file, 'read');
+		var str = stream.readUTFBytes(stream.bytesAvailable);
+		stream.close();
+		return str;
+	}
+
 });
 
 File.protocols = {
@@ -9,6 +89,7 @@ File.protocols = {
 };
 
 File.resolve = function(path){
+	if (path.nativePath) return path; // better check?
 	return $try(function(){
 		return new air.File(path);
 	}, function(){
