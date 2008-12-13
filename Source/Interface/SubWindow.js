6690955338fe5d7d45c863985ca6f91e83ef41ce
Jan Kassens <jan@kassens.net>
2008-12-13 20:09:25 +0100

    adding initial version of the new SubWindow class and a simple demo.

---
+++ Source/Interface/SubWindow.js
@@ -0,0 +1,19 @@
+var SubWindow = new Class({
+	
+	initialize: function(content){
+		var loader = air.HTMLLoader.createRootWindow();
+		this.nativeWindow = loader.window.nativeWindow;
+		if (content.test(/[a-z]+:/)){
+			var file = Filesystem.resolve(content);
+			if (file) content = file.url;
+			loader.load(new air.URLRequest(content));
+		}
+		else loader.loadString(content);
+	},
+	
+	close: function(){
+		this.nativeWindow.close();
+		return this;
+	}
+	
+});
