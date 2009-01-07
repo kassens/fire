593774f7468cab4d2a7759fc7e891890778013ac
Jan Kassens <jan@kassens.net>
2009-01-07 12:24:11 +0100

    adding options to AppWindow

--- Demos/AppWindow/demo.js
+++ Demos/AppWindow/demo.js
@@ -1,6 +1,14 @@
-new AppWindow('http://www.google.com');
+new AppWindow('http://www.google.com', {bounds: {
+	top: 400, left: 400
+}});
 new AppWindow('app:/Demos/AppWindow/second.html');
-var win = new AppWindow('Content from a <code>string</code>. Closes after 10 seconds.');
+var win = new AppWindow('Content from a <code>string</code>. Closes after 10 seconds.', {
+	systemChrome: false,
+	type: 'lightweight',
+	bounds: {
+		top: 100, left: 100
+	}
+});
 (function(){
 	win.close();
 }).delay(10000);
--- Source/Interface/AppWindow.js
+++ Source/Interface/AppWindow.js
@@ -1,19 +1,54 @@
 var AppWindow = new Class({
-	
-	initialize: function(content){
-		var loader = air.HTMLLoader.createRootWindow();
+
+	Implements: Options,
+
+	options: {
+		maximizable: true,
+		minimizable: true,
+		resizable: true,
+		systemChrome: true,
+		transparent: false,
+		type: 'normal', // utility, lightweight
+
+		visible: true,
+		scrollbars: true,
+
+		bounds: {
+			top: 300,
+			left: 300,
+			width: 300,
+			height: 200
+		}
+	},
+
+	initialize: function(content, options){
+		this.setOptions(options);
+
+		var windowOptions = new air.NativeWindowInitOptions();
+		windowOptions.systemChrome = this.options.systemChrome ? 'standard' : 'none';
+		['maximizable', 'minimizable', 'resizable', 'transparent', 'type'].each(function(option){
+			windowOptions[option] = this.options[option];
+		}, this);
+
+		// TODO: should use setBounds
+		var bounds = new air.Rectangle(
+			this.options.bounds.top, this.options.bounds.left,
+			this.options.bounds.width, this.options.bounds.height);
+
+		var loader = air.HTMLLoader.createRootWindow(this.options.visible, windowOptions, this.options.scrollbars, bounds);
 		this.nativeWindow = loader.window.nativeWindow;
 		if (content.test(/[a-z]+:/)){
 			var file = Filesystem.resolve(content);
 			if (file) content = file.url;
 			loader.load(new air.URLRequest(content));
+		} else {
+			loader.loadString(content);
 		}
-		else loader.loadString(content);
 	},
-	
+
 	close: function(){
 		this.nativeWindow.close();
 		return this;
 	}
-	
+
 });
