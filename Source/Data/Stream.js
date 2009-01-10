70c6576859373ee85192ba05f428f5c351fe8d58
Keeto Obcena <markeeto@gmail.com>
2009-01-11 00:50:11 +0800

    Added new Stream.$props and Stream.$events.

--- Source/Data/Stream.js
+++ Source/Data/Stream.js
@@ -22,7 +22,7 @@ var Stream = new Class({
 	initialize: function(stream){
 		this.stream = stream;
 		
-		Stream.extend(this);
+		if (!Stream.extend(this)) return false;
 	},
 	
 	write: function(data, type){
@@ -63,31 +63,45 @@ var Stream = new Class({
 // doesn't want us iterating through its objects.
 Stream.$props = $H({
 	"bytesAvailable": [],
-	"connected": [1, 2],
+	"connected": [1, 3],
 	"endian": [],
 	"objectEncoding": [],
+	"position": [2],
+	"readAhead": [2],
 	"timeout": [1],
-	"close": [1],
+	"close": [],
+	"load": [3],
+	"open": [2],
+	"openAsync": [2],
 	"connect": [1],
-	"flush": [1]
+	"flush": [1],
+	"truncate": [2]
 });
 
 Stream.$events = $H({
 	"close": [],
-	"connect": [],
+	"complete": [2, 3],
+	"connect": [1],
+	"httpResponseStatus": [3],
+	"httpStatus": [3],
 	"ioError": [],
-	"securityError": [],
-	"socketData": []
+	"open": [3],
+	"outputProgress": [2],
+	"progress": [2],
+	"securityError": [1, 3],
+	"socketData": [1]
 });
 
 Stream.extend = function(obj){
 	var objType = 0;
 	if (obj.stream.constructor == air.Socket) objType = 1;
 	else if (obj.stream.constructor == air.FileStream) objType = 2;
+	else if (obj.stream.constructor == air.FileStream) objType = 3;
+	else return false;
 	
 	Stream.$props.each(function(types, prop){
 		if (types.length == 0 || types.contains(objType)) {
-			obj.__defineGetter__(prop, function() {
+			obj.__defineGetter__(prop, function(){
 				return obj.stream[prop];
 			});
 			obj.__defineSetter__(prop, function(value) {
@@ -103,4 +117,6 @@ Stream.extend = function(obj){
 			});
 		}
 	});
+	
+	return true;
 };
