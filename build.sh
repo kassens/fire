316aa29e7ea3d713570c5742c092b8bba19aff18
Jan Kassens <jan@kassens.net>
2009-01-26 21:33:35 +0100

    adding new complete build shell script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cat Source/Core/Aliases.js > fire.js
+cat Source/Data/Stream.js >> fire.js
+cat Source/Data/File.js >> fire.js
+cat Source/Data/Secure.js >> fire.js
+cat Source/Data/Hash.Secure.js >> fire.js
+cat Source/Database/Database.js >> fire.js
+cat Source/Database/Database.Query.js >> fire.js
+cat Source/Fx/Fx.Window.js >> fire.js
+cat Source/Interface/AppWindow.js >> fire.js
+cat Source/Interface/Menu.js >> fire.js
+cat Source/Network/Socket.js >> fire.js
